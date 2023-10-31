8f9f8f51fa780da1857bebf8a3c8fd1ebbc3e4c4
GiftAregbesola <giftaregbesola@gmail.com>
2023-10-31 21:08:30 +0100

    mywork

---
+++ my_AI_journey/learning_python/file.py
@@ -0,0 +1,18 @@
+temp= eval(input("Enter a tempersture in celsius: "))
+print("In Fahrenheit, that is", 9/5*temp+32)
+num1=eval(input("Enter the first number: "))
+num2= eval(input("Enter the second number: "))
+print("The average of thenumbers you entered is", (num1+num2)/2)
+temp= eval(input("Enter a tempersture in Celsius: "))
+f_temp= 9/5*temp+32
+print("InFahrenheit, that is",f_temp)
+if f_temp > 212:
+    print("That temperature is above the boiling point.")
+if f_temp < 32:
+    print("That tempersture is below the freezing point.") 
+x=3
+y=4
+z=x+y
+z=z+1
+x=y
+y=5   
---
+++ my_AI_journey/learning_python/note.py
@@ -0,0 +1,7 @@
+temp= eval(input("Enter a tempersture in Celsius: "))
+f_temp= 9/5*temp+32
+print("InFahrenheit, that is",f_temp)
+if f_temp > 212:
+    print("That temperature is above the boiling point.")
+if f_temp < 32:
+    print("That tempersture is below the freezing point.") 
