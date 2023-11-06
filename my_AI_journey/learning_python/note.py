d2d6517d3e8dc5cd5b761934604a7cca982330fc
GiftAregbesola <giftaregbesola@gmail.com>
2023-11-06 20:53:04 +0100

    my_work

--- my_AI_journey/learning_python/note.py
+++ my_AI_journey/learning_python/note.py
@@ -4,4 +4,4 @@ print("InFahrenheit, that is",f_temp)
 if f_temp > 212:
     print("That temperature is above the boiling point.")
 if f_temp < 32:
-    print("That tempersture is below the freezing point.") 
+    print("That tempersture is below the freezing point.")
