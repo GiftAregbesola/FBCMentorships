b8a4469721f1d0c6ae60954514a68430b2d0e644
GiftAregbesola <giftaregbesola@gmail.com>
2023-11-08 01:46:13 +0100

    my_work

--- my_AI_journey/learning_python/week_2/day_0_2.py
+++ my_AI_journey/learning_python/week_2/day_0_2.py
@@ -12,4 +12,3 @@ print("cos(0)= ", cos(0))
 print(abs(-4.3))
 print(round(3.336, 2))
 print(round(345.2, -1))
- 
---
+++ my_AI_journey/learning_python/week_2/day_1_1.py
@@ -0,0 +1,5 @@
+length= eval(input("Enter a length in centimeter: "))
+if length<0:
+    print("Invalid Entry")
+else:
+    print(length*2.54)
